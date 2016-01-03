8bbed44c184f435c7a066d1e4698a924b1a80d0e
Marcus Spangenberg <marcus.spangenberg@gmail.com>
2016-01-03 15:31:04 +0100

    Added tests

---
+++ Test/LWMessageQueueTest.cpp
@@ -0,0 +1,220 @@
+#include <assert.h>
+#include <iostream>
+#include <memory>
+#include <thread>
+#include <vector>
+#include "LWMessageQueue.h"
+
+namespace {
+
+class ScopedFunctionTrace {
+public:
+	ScopedFunctionTrace(const char* inFunctionName)
+	: functionName(inFunctionName)
+	{
+		std::cout << "-- " << functionName << " begin" << std::endl;		
+	}
+	~ScopedFunctionTrace() {
+		std::cout << "   " << functionName << " end" << std::endl;
+	}
+private:
+	std::string functionName;
+};
+
+struct Message1 {
+	uint32_t value;
+};
+
+struct Message2 {
+	char charValue;
+	uint32_t uintValue;
+};
+
+struct Message3 {
+	char* stringRef;
+	uint32_t uintValue;
+	double doubleValue;
+};
+
+union MessageUnion {
+	Message1 message1;
+	Message2 message2;
+	Message3 message3;
+};
+
+enum class MessageType {
+	Message1,
+	Message2,
+	Message3
+};
+
+} // namespace
+
+void pushMessageTest() {
+	ScopedFunctionTrace(__FUNCTION__);
+
+	using MessageQueue = LWMessageQueue::LWMessageQueue<1, 1, MessageUnion, MessageType>;
+	std::unique_ptr<MessageQueue> messageQueue(new MessageQueue());
+
+	MessageQueue::ThreadChannelOutput channelOutput = messageQueue->getThreadChannelOutput(0);
+	MessageQueue::ThreadChannelInput channelInput = messageQueue->getThreadChannelInput(0);
+	
+	assert(channelOutput.getNumMessages() == 0);
+
+	Message1 message;
+	message.value = 0;
+	channelInput.pushMessage(message, MessageType::Message1);
+
+	assert(channelOutput.getNumMessages() == 1);
+}
+
+void popMessageTest() {
+	ScopedFunctionTrace(__FUNCTION__);
+
+	using MessageQueue = LWMessageQueue::LWMessageQueue<1, 1, MessageUnion, MessageType>;
+	std::unique_ptr<MessageQueue> messageQueue(new MessageQueue());
+
+	MessageQueue::ThreadChannelOutput channelOutput = messageQueue->getThreadChannelOutput(0);
+	MessageQueue::ThreadChannelInput channelInput = messageQueue->getThreadChannelInput(0);
+
+	Message2 message;
+	message.charValue = 3;
+	message.uintValue = 5;
+	channelInput.pushMessage(message, MessageType::Message2);
+
+	MessageQueue::MessageContainer messageContainer = channelOutput.popMessage();
+	assert(channelOutput.getNumMessages() == 0);
+
+	assert(messageContainer.getType() == MessageType::Message2);
+	const Message2& poppedMessage = messageContainer.getMessage<Message2>();
+	assert(poppedMessage.charValue == 3);
+	assert(poppedMessage.uintValue == 5);
+}
+
+void isFullTest() {
+	ScopedFunctionTrace(__FUNCTION__);
+
+	using MessageQueue = LWMessageQueue::LWMessageQueue<2, 2, MessageUnion, MessageType>;
+	std::unique_ptr<MessageQueue> messageQueue(new MessageQueue());
+
+	MessageQueue::ThreadChannelOutput channel0Output = messageQueue->getThreadChannelOutput(0);
+	MessageQueue::ThreadChannelInput channel0Input = messageQueue->getThreadChannelInput(0);
+	MessageQueue::ThreadChannelOutput channel1Output = messageQueue->getThreadChannelOutput(1);
+	MessageQueue::ThreadChannelInput channel1Input = messageQueue->getThreadChannelInput(1);
+
+	Message1 message;
+	message.value = 0;
+	channel0Input.pushMessage(message, MessageType::Message1);
+	channel1Input.pushMessage(message, MessageType::Message1);
+
+	assert(!channel0Input.isFull());
+	assert(!channel1Input.isFull());
+
+	channel0Input.pushMessage(message, MessageType::Message1);
+	channel1Input.pushMessage(message, MessageType::Message1);
+
+	assert(channel0Input.isFull());
+	assert(channel1Input.isFull());
+
+	MessageQueue::MessageContainer messageContainer = channel0Output.popMessage();
+	messageContainer = channel0Output.popMessage();
+	messageContainer = channel1Output.popMessage();
+	messageContainer = channel1Output.popMessage();
+
+	assert(!channel0Input.isFull());
+	assert(!channel1Input.isFull());
+}
+
+namespace MultiThreadTest {
+
+const uint32_t queueSize = 1048576;
+const uint32_t numInputThreads = 10;
+using MessageQueue = LWMessageQueue::LWMessageQueue<queueSize, numInputThreads, MessageUnion, MessageType>;
+
+void inputThreadEntry(MessageQueue::ThreadChannelInput inThreadChannelInput, const uint32_t inQueueSize, const uint32_t inChannelIndex) {
+	Message1 message1;
+	Message2 message2;
+
+	for (uint32_t i = 0; i < inQueueSize; i += 2) {
+		message1.value = inChannelIndex;
+		message2.uintValue = inChannelIndex;
+		message2.charValue = static_cast<char>(inChannelIndex);
+		inThreadChannelInput.pushMessage(message1, MessageType::Message1);
+		inThreadChannelInput.pushMessage(message2, MessageType::Message2);
+	}
+}
+
+void verifyMessage(MessageQueue::MessageContainer&& inMessageContainer, const uint32_t inChannelIndex) {
+	switch (inMessageContainer.getType()) {
+	case MessageType::Message1:
+		{
+			const auto& message = inMessageContainer.getMessage<Message1>();
+			assert(message.value == inChannelIndex);
+		}
+		break;
+	case MessageType::Message2:
+		{
+			const auto& message = inMessageContainer.getMessage<Message2>();
+			assert(message.uintValue == inChannelIndex && message.charValue == static_cast<char>(inChannelIndex));
+		}
+		break;
+	default:
+		assert(false);
+		break;
+	}
+}
+
+void outputThreadEntry(std::shared_ptr<MessageQueue> inMessageQueue, const uint32_t inQueueSize, const uint32_t inNumInputThreads) {
+	std::vector<MessageQueue::ThreadChannelOutput> channelOutputs;
+	channelOutputs.reserve(inNumInputThreads);
+
+	for (uint32_t channelIndex = 0; channelIndex < inNumInputThreads; ++channelIndex) {
+		channelOutputs.push_back(inMessageQueue->getThreadChannelOutput(channelIndex));
+	}
+
+	const uint32_t totalMessages = inQueueSize * inNumInputThreads;
+	uint32_t receivedMessages = 0;
+
+	while (receivedMessages < totalMessages) {
+		for (uint32_t channelIndex = 0; channelIndex < inNumInputThreads; ++channelIndex) {
+			MessageQueue::ThreadChannelOutput& channelOutput = channelOutputs[channelIndex];
+			if (channelOutput.getNumMessages() != 0) {
+				verifyMessage(channelOutput.popMessage(), channelIndex);
+				++receivedMessages;
+			}
+		}
+	}
+	std::cout << "   Output thread received " << receivedMessages << " messages" << std::endl;
+}
+
+void multiThreadTest() {
+	ScopedFunctionTrace(__FUNCTION__);
+
+	std::shared_ptr<MessageQueue> messageQueue(new MessageQueue());
+
+	std::vector<std::unique_ptr<std::thread>> inputThreads;
+	inputThreads.reserve(numInputThreads);
+
+	for (uint32_t channelIndex = 0; channelIndex < numInputThreads; ++channelIndex) {
+		inputThreads.emplace_back(new std::thread(inputThreadEntry, messageQueue->getThreadChannelInput(channelIndex), queueSize, channelIndex));
+	}
+
+	std::unique_ptr<std::thread> outputThread(new std::thread(outputThreadEntry, messageQueue, queueSize, numInputThreads));
+
+	for (auto& inputThread : inputThreads) {
+		inputThread->join();
+	}
+	outputThread->join();
+}
+
+} // namespace MultiThreadTest
+
+int main(int, char**) {
+	pushMessageTest();
+	popMessageTest();
+	isFullTest();
+	MultiThreadTest::multiThreadTest();
+
+	std::cout << "Tests passed" << std::endl;
+	return 0;
+}
